12933671e486630e5b1f8e0579a252587ab4604c
Mersedeh Sadeghi <mersedeh.sadeghi@mail.polimi.it>
2020-10-31 15:53:32 +0100

    feat basic funcs

---
+++ SimpleAddExample/src/it/polimi/mer/beans/ShoppingList_Bean.java
@@ -0,0 +1,31 @@
+package it.polimi.mer.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateful;
+
+import it.polimi.mer.interfaces.ShoppingList_Interface;
+
+@Stateful
+public class ShoppingList_Bean implements ShoppingList_Interface {
+	
+	List<String> products= new ArrayList();
+
+	@Override
+	public void addNewProduct(String p) {
+		products.add(p);
+	}
+
+	@Override
+	public void removeProducts(String p) {
+		products.remove(p);	
+	}
+
+	@Override
+	public List<String> getProducts() {
+		return products;
+	}
+	
+
+}
---
+++ SimpleAddExample/src/it/polimi/mer/interfaces/ShoppingList_Interface.java
@@ -0,0 +1,15 @@
+package it.polimi.mer.interfaces;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface ShoppingList_Interface {
+	
+	public void addNewProduct(String p); 
+	
+	public void removeProducts(String p);
+
+	public List<String> getProducts();
+}
